e88a944484fb6294f8306ec27905013f699e3a11
IgorP25 <igor@zimp.com.au>
2025-01-24 03:55:55 +1100

    Added movement controls to player

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from constants import *
 from player import *
 
 def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -20,11 +24,6 @@ def main():
         player.draw(screen)
         pygame.display.flip()
         dt = game_clock.tick(60) / 1000
-
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
     
 
 if __name__ == "__main__":
